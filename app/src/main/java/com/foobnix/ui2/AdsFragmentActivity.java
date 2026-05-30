b77e2a91998414c1b88b09e3e0729ee97655ea0c
yuriluv <yuriluv@users.noreply.github.com>
2026-05-31 01:29:36 +0900

    Restore main back press handling in ad stub

--- app/src/main/java/com/foobnix/ui2/AdsFragmentActivity.java
+++ app/src/main/java/com/foobnix/ui2/AdsFragmentActivity.java
@@ -1,13 +1,33 @@
 package com.foobnix.ui2;
 
-import android.content.Context;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+
+import androidx.activity.OnBackPressedCallback;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class AdsFragmentActivity extends AppCompatActivity {
     private boolean rewardLoaded = false;
     private boolean rewardActivated = true;
+    private Handler handler;
+    private boolean doubleBackToExitPressedOnce = false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        handler = new Handler(Looper.getMainLooper());
+        getOnBackPressedDispatcher().addCallback(new OnBackPressedCallback(true) {
+            @Override
+            public void handleOnBackPressed() {
+                onBackPressedAction();
+            }
+        });
+    }
 
     public void showInterstitial() {
+        onFinishActivity();
     }
 
     public void showInterstitialNoFinish() {
@@ -28,5 +48,40 @@ public class AdsFragmentActivity extends AppCompatActivity {
     }
 
     public void showRewardVideo(Runnable callback) {
+        if (callback != null) {
+            callback.run();
+        }
+    }
+
+    public void onBackPressedAction() {
+        if (doubleBackToExitPressedOnce) {
+            if (handler != null) {
+                handler.removeCallbacksAndMessages(null);
+            }
+            onBackPressedFinishImpl();
+            return;
+        }
+        doubleBackToExitPressedOnce = true;
+
+        if (this instanceof MainTabs2 && handler != null) {
+            handler.postDelayed(() -> {
+                doubleBackToExitPressedOnce = false;
+                onBackPressedImpl();
+            }, 500);
+        } else {
+            onBackPressedImpl();
+        }
+    }
+
+    public void onBackPressedImpl() {
+        finish();
+    }
+
+    public void onBackPressedFinishImpl() {
+        onFinishActivity();
+    }
+
+    public void onFinishActivity() {
+        finish();
     }
 }
